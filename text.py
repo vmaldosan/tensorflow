f6c42b05d8a3c8e0eaedfa431a5e2a7f35a0b255
Victor Maldonado <vmaldosan@gmail.com>
2022-12-23 18:15:31 +0000

    Download dataset

---
+++ text.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import os
+import re
+import shutil
+import string
+import tensorflow as tf
+
+from tensorflow.keras import layers
+from tensorflow.keras import losses
+
+
+# print (tf.__version__)
+
+# url = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
+
+# dataset = tf.keras.utils.get_file("aclImdb_v1", url, untar=True, cache_dir='.', cache_subdir='')
+dataset_dir = os.path.join(os.path.dirname('.'), 'aclImdb') # os.path.join(os.path.dirname(dataset), 'aclImdb')
+
+print(os.listdir(dataset_dir))
+
+train_dir = os.path.join(dataset_dir, 'train')
+print(os.listdir(train_dir))
+
+# sample_file = os.path.join(train_dir, 'pos/1181_9.txt')
+# with open(sample_file) as f:
+    # print(f.read())
+
