6b131b44bcb46517cd1966bb124813ec8304ba0b
Victor Maldonado <vmaldosan@gmail.com>
2022-12-29 21:51:33 +0000

    Preprocess data

--- text.py
+++ text.py
@@ -40,3 +40,32 @@ raw_test_ds = tf.keras.utils.text_dataset_from_directory(
     'aclImdb/test',
     batch_size=batch_size)
 
+def custom_standardization(input_data):
+    lowercase = tf.strings.lower(input_data)
+    stripped_html = tf.strings.regex_replace(lowercase, '<br />', ' ')
+    return tf.strings.regex_replace(stripped_html, '[%s]' % re.escape(string.punctuation), '')
+
+max_features = 10000
+sequence_length = 250
+
+vectorize_layer = layers.TextVectorization(
+    standardize=custom_standardization,
+    max_tokens=max_features,
+    output_mode='int',
+    output_sequence_length=sequence_length
+)
+
+# Make a text-only dataset (without labels), then call adapt
+train_text = raw_train_ds.map(lambda x, y: x)
+vectorize_layer.adapt(train_text)
+
+def vectorize_text(text, label):
+    text = tf.expand_dims(text, -1)
+    return vectorize_layer(text), label
+
+# retrieve a batch (of 32 reviews and labels) from the dataset
+text_batch, label_batch = next(iter(raw_train_ds))
+first_review, first_label = text_batch[0], label_batch[0]
+print('Review: ', first_review)
+print('Label: ', raw_train_ds.class_names[first_label])
+print('Vectorized review: ', vectorize_text(first_review, first_label))
