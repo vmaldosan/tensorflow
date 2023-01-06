6a813afa1939ba3899716523ff7177c105e03c51
vmaldosan <vmaldosan@gmail.com>
2023-01-06 00:12:36 +0000

    Create plot of accuracy and loss

--- text.py
+++ text.py
@@ -14,6 +14,7 @@
 # 7. Use a loss function and an optimizer
 # 8. Train the model
 # 9. Evaluate the model
+# 10. Create a plot of accuracy and loss over time
 #
 ###################################################################################################
  
@@ -134,3 +135,31 @@ history = model.fit(
 loss, accuracy = model.evaluate(test_ds)
 print('Loss: ', loss)
 print('Accuracy: ', accuracy)
+
+# Create a plot of accuracy and loss over time
+history_dict = history.history
+# print(history_dict.keys())
+acc = history_dict['binary_accuracy']
+val_acc = history_dict['val_binary_accuracy']
+loss = history_dict['loss']
+val_loss = history_dict['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+# "bo" is for "blue dot"
+plt.plot(epochs, loss, 'bo', label='Training loss')
+# b is for "solid blue line"
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+plt.show()
