5ee0cde0f654b8404392f30e234161270771c199
vmaldosan <vmaldosan@gmail.com>
2023-01-24 22:40:38 +0000

    Create model for prediction

--- intro.py
+++ intro.py
@@ -1,8 +1,22 @@
 import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
 
 melbourne_data = pd.read_csv('basic-data-exploration/melb_data.csv')
-melbourne_data.describe()
+# melbourne_data.describe()
+melbourne_data.columns
 
-home_data = pd.read_csv('basic-data-exploration/train.csv')
-home_data.describe()
+# dropna drops missing values
+melbourne_data.dropna(axis=0)
+y = melbourne_data.Price
+X = melbourne_data[['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longitude']]
+X.describe()
 
+# Define model, specifying a constant random_state to ensure same results on each run
+melbourne_model = DecisionTreeRegressor(random_state=7)
+# Fit model
+melbourne_model.fit(X, y)
+
+print('Making predictions for the following 5 houses:')
+print(X.head())
+print('The predictions are:')
+print(melbourne_model.predict(X.head()))
