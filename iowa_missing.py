03fa101a567e39d233a720d38ea98e7fc371fd4d
vmaldosan <vmaldosan@gmail.com>
2023-02-05 23:41:50 +0000

    Dropping columns with missing data

---
+++ iowa_missing.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
+# Load the data, and separate the target
+X_full = pd.read_csv('./iowa/train.csv', index_col='Id')
+X_test_full = pd.read_csv('./iowa/test.csv', index_col='Id')
+
+# Remove rows with missing target, separate target from predictors
+X_full.dropna(axis=0, subset=['SalePrice'], inplace=True)
+y = X_full.SalePrice
+X_full.drop(['SalePrice'], axis=1, inplace=True)
+
+# To keep things simple, we'll use only numerical predictors
+X = X_full.select_dtypes(exclude=['object'])
+X_test = X_test_full.select_dtypes(exclude=['object'])
+
+# Break off validation set from training data
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2, 
+                                                      random_state=0)
+print(X_train.head())
+
+# Shape of training data (num_rows, num_columns)
+print(X_train.shape)
+
+# Number of missing values in each column of training data
+missing_val_count_by_column = (X_train.isnull().sum())
+print(missing_val_count_by_column[missing_val_count_by_column > 0])
+
+# Function for comparing different approaches
+def score_dataset(X_train, X_valid, y_train, y_valid):
+    model = RandomForestRegressor(n_estimators=100, random_state=0)
+    model.fit(X_train, y_train)
+    preds = model.predict(X_valid)
+    return mean_absolute_error(y_valid, preds)
+
+cols_with_missing = [col for col in X_train.columns
+                    if X_train[col].isnull().any()]
+
+reduced_X_train = X_train.drop(cols_with_missing, axis=1)
+reduced_X_valid = X_valid.drop(cols_with_missing, axis=1)
+
+print("MAE (Drop columns with missing values):")
+print(score_dataset(reduced_X_train, reduced_X_valid, y_train, y_valid))
